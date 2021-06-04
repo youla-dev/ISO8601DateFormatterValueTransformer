7fe4e84a39a42c40f3e03ea3ff39a3ccb4551c2b
Alexander Skandakov <a.skandakov@corp.mail.ru>
2021-06-04 14:10:54 +0300

    spm support

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+let package = Package(
+    name: "ISO8601DateFormatterValueTransformer",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        .library(
+            name: "ISO8601DateFormatterValueTransformer",
+            targets: ["ISO8601DateFormatterValueTransformer"])
+    ],
+    dependencies: [
+        .package(name:"RKValueTransformers" ,url: "https://github.com/youla-dev/RKValueTransformers", .branch("xcframework")),
+    ],
+    targets: [
+        .target(
+            name: "ISO8601DateFormatterValueTransformer",
+            dependencies: [
+                .byName(name: "RKValueTransformers"),
+            ],
+            path: "Code/"
+        ),
+    ])
