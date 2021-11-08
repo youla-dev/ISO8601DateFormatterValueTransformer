b983d70e3ad1ed1485fb24926485e025e5be60d9
Alexander Skandakov <a.skandakov@corp.mail.ru>
2021-11-08 14:28:37 +0300

    update RKValueTransformers

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["ISO8601DateFormatterValueTransformer"])
     ],
     dependencies: [
-        .package(name:"RKValueTransformers" ,url: "https://github.com/youla-dev/RKValueTransformers", .revision("82e881378ffd746b168ffbdb7deab8b8c1c4603a")),
+        .package(name:"RKValueTransformers" ,url: "https://github.com/youla-dev/RKValueTransformers", .revision("203a968b9a8f00e23b2c74285f7129081a6fa4a0")),
     ],
     targets: [
         .target(
